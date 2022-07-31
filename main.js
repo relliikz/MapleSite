fca331d20301988dd56513c8b7638c5049e494ad
Ben Pritchard <101098271@student.swin.edu.au>
2022-07-31 23:25:11 +1000

    initial

---
+++ main.js
@@ -0,0 +1,410 @@
+
+//totem dropdown functions
+
+function changeT0(WhichTotem){
+    if(WhichTotem == "tOne0"){
+    document.getElementById('totemOne').innerHTML = "Totem 1 ";
+    } else if(WhichTotem == "tTwo0"){
+    document.getElementById('totemTwo').innerHTML = "Totem 2 ";
+    } else if(WhichTotem == "tThree0"){
+      document.getElementById('totemThree').innerHTML = "Totem 3 ";
+    }
+  }
+  function changeT1(WhichTotem){
+    if(WhichTotem == "tOne1"){
+    document.getElementById('totemOne').innerHTML = "Dark Totem";
+    } else if(WhichTotem == "tTwo1"){
+    document.getElementById('totemTwo').innerHTML = "Dark Totem";
+    } else if(WhichTotem == "tThree1"){
+      document.getElementById('totemThree').innerHTML = "Dark Totem";
+    }
+  }
+  function changeT2(WhichTotem){
+    if(WhichTotem == "tOne2"){
+    document.getElementById('totemOne').innerHTML = "Gold Totem";
+    } else if(WhichTotem == "tTwo2"){
+    document.getElementById('totemTwo').innerHTML = "Gold Totem";
+    } else if(WhichTotem == "tThree2"){
+      document.getElementById('totemThree').innerHTML = "Gold Totem";
+    }
+  }
+  function changeT3(WhichTotem){
+    if(WhichTotem == "tOne3"){
+    document.getElementById('totemOne').innerHTML = "Silver Totem ";
+    } else if(WhichTotem == "tTwo3"){
+    document.getElementById('totemTwo').innerHTML = "Silver Totem";
+    } else if(WhichTotem == "tThree3"){
+      document.getElementById('totemThree').innerHTML = "Silver Totem";
+    }
+  }
+  function changeT4(WhichTotem){
+    if(WhichTotem == "tOne4"){
+    document.getElementById('totemOne').innerHTML = "Afterlands Totem";
+    } else if(WhichTotem == "tTwo4"){
+    document.getElementById('totemTwo').innerHTML = "Afterlands Totem";
+    } else if(WhichTotem == "tThree4"){
+      document.getElementById('totemThree').innerHTML = "Afterlands Totem";
+    }
+  }
+
+//ring dropdown functions
+
+  function changeR0(WhichRing){
+    if(WhichRing == "rOne0"){
+    document.getElementById('ringOne').innerHTML = "Ring 1 ";
+    } else if(WhichRing == "rTwo0"){
+    document.getElementById('ringTwo').innerHTML = "Ring 2 ";
+    } else if(WhichRing == "rThree0"){
+      document.getElementById('ringThree').innerHTML = "Ring 3 ";
+    } else if(WhichRing == "rFour0"){
+      document.getElementById('ringFour').innerHTML = "Ring 4 ";
+  }
+}
+function changeR1(WhichRing){
+  if(WhichRing == "rOne1"){
+  document.getElementById('ringOne').innerHTML = "Endless Terror";
+  } else if(WhichRing == "rTwo1"){
+  document.getElementById('ringTwo').innerHTML = "Endless Terror";
+  } else if(WhichRing == "rThree1"){
+    document.getElementById('ringThree').innerHTML = "Endless Terror";
+  } else if(WhichRing == "rFour1"){
+    document.getElementById('ringFour').innerHTML = "Endless Terror";
+  }
+}
+function changeR2(WhichRing){
+  if(WhichRing == "rOne2"){
+  document.getElementById('ringOne').innerHTML = "Guardian Angel Slime Ring";
+  } else if(WhichRing == "rTwo2"){
+  document.getElementById('ringTwo').innerHTML = "Guardian Angel Slime Ring";
+  } else if(WhichRing == "rThree2"){
+    document.getElementById('ringThree').innerHTML = "Guardian Angel Slime Ring";
+  } else if(WhichRing == "rFour2"){
+    document.getElementById('ringFour').innerHTML = "Guardian Angel Slime Ring";
+  }
+}
+function changeR3(WhichRing){
+  if(WhichRing == "rOne3"){
+  document.getElementById('ringOne').innerHTML = "Superior Gollux Ring";
+  } else if(WhichRing == "rTwo3"){
+  document.getElementById('ringTwo').innerHTML = "Superior Gollux Ring";
+  } else if(WhichRing == "rThree3"){
+    document.getElementById('ringThree').innerHTML = "Superior Gollux Ring";
+  } else if(WhichRing == "rFour3"){
+    document.getElementById('ringFour').innerHTML = "Superior Gollux Ring";
+  }
+}
+function changeR4(WhichRing){
+  if(WhichRing == "rOne4"){
+  document.getElementById('ringOne').innerHTML = "Reinforced Gollux Ring";
+  } else if(WhichRing == "rTwo4"){
+  document.getElementById('ringTwo').innerHTML = "Reinforced Gollux Ring";
+  } else if(WhichRing == "rThree4"){
+    document.getElementById('ringThree').innerHTML = "Reinforced Gollux Ring";
+  } else if(WhichRing == "rFour4"){
+    document.getElementById('ringFour').innerHTML = "Reinforced Gollux Ring";
+  }
+}
+function changeR5(WhichRing){
+  if(WhichRing == "rOne5"){
+  document.getElementById('ringOne').innerHTML = "Solid Gollux Ring";
+  } else if(WhichRing == "rTwo5"){
+  document.getElementById('ringTwo').innerHTML = "Solid Gollux Ring";
+  } else if(WhichRing == "rThree5"){
+    document.getElementById('ringThree').innerHTML = "Solid Gollux Ring";
+  } else if(WhichRing == "rFour5"){
+    document.getElementById('ringFour').innerHTML = "Solid Gollux Ring";
+  }
+}
+function changeR6(WhichRing){
+  if(WhichRing == "rOne6"){
+  document.getElementById('ringOne').innerHTML = "Kanna's Treasure";
+  } else if(WhichRing == "rTwo6"){
+  document.getElementById('ringTwo').innerHTML = "Kanna's Treasure";
+  } else if(WhichRing == "rThree6"){
+    document.getElementById('ringThree').innerHTML = "Kanna's Treasure";
+  } else if(WhichRing == "rFour6"){
+    document.getElementById('ringFour').innerHTML = "Kanna's Treasure";
+  }
+}
+function changeR7(WhichRing){
+  if(WhichRing == "rOne7"){
+  document.getElementById('ringOne').innerHTML = "Meister Ring";
+  } else if(WhichRing == "rTwo7"){
+  document.getElementById('ringTwo').innerHTML = "Meister Ring";
+  } else if(WhichRing == "rThree7"){
+    document.getElementById('ringThree').innerHTML = "Meister Ring";
+  } else if(WhichRing == "rFour7"){
+    document.getElementById('ringFour').innerHTML = "Meister Ring";
+  }
+}
+function changeR8(WhichRing){
+  if(WhichRing == "rOne8"){
+  document.getElementById('ringOne').innerHTML = "Oz Ring (ROR4, WJ4,RT4)";
+  } else if(WhichRing == "rTwo8"){
+  document.getElementById('ringTwo').innerHTML = "Oz Ring (ROR4, WJ4,RT4)";
+  } else if(WhichRing == "rThree8"){
+    document.getElementById('ringThree').innerHTML = "Oz Ring (ROR4, WJ4,RT4)";
+  } else if(WhichRing == "rFour8"){
+    document.getElementById('ringFour').innerHTML = "Oz Ring (ROR4, WJ4,RT4)";
+  }
+}
+
+//Accessory dropdown functions
+
+  function changeA0(WhichAccessory){ 
+    if(WhichAccessory == "aOne0"){
+      document.getElementById('faceOne').innerHTML = "Face ";
+      } else if(WhichAccessory == "aTwo0"){
+      document.getElementById('eyeOne').innerHTML = "Eye ";
+      } else if(WhichAccessory == "aThree0"){
+    document.getElementById('pendantOne').innerHTML = "Pendant1 ";
+    } else if(WhichAccessory == "aFour0"){
+    document.getElementById('pendantTwo').innerHTML = "Pendant 2 ";
+    } else if(WhichAccessory == "aFive0"){
+      document.getElementById('beltOne').innerHTML = "Belt ";
+    } else if(WhichAccessory == "aSix0"){
+      document.getElementById('earringOne').innerHTML = "Earring ";
+  }
+}
+function changeA1(WhichAccessory){ 
+  if(WhichAccessory == "aOne1"){
+    document.getElementById('faceOne').innerHTML = "Berserked";
+    } else if(WhichAccessory == "aTwo1"){
+    document.getElementById('eyeOne').innerHTML = "Magic Eyepatch";
+    } else if(WhichAccessory == "aThree1"){
+  document.getElementById('pendantOne').innerHTML = "Superior Gollux Pendant";
+  } else if(WhichAccessory == "aFour1"){
+  document.getElementById('pendantTwo').innerHTML = "Superior Gollux Pendant";
+  } else if(WhichAccessory == "aFive1"){
+    document.getElementById('beltOne').innerHTML = "Superior Gollux Belt";
+  } else if(WhichAccessory == "aSix1"){
+    document.getElementById('earringOne').innerHTML = "Superior Gollux Earring";
+}
+}
+function changeA2(WhichAccessory){ 
+  if(WhichAccessory == "aOne2"){
+    document.getElementById('faceOne').innerHTML = "Sweetwater Tattoo";
+    } else if(WhichAccessory == "aTwo2"){
+    document.getElementById('eyeOne').innerHTML = "Sweetwater Monocle (Transposed)";
+    } else if(WhichAccessory == "aThree2"){
+  document.getElementById('pendantOne').innerHTML = "Sweetwater Pendant (Transposed)";
+  } else if(WhichAccessory == "aFour2"){
+  document.getElementById('pendantTwo').innerHTML = "Sweetwater Pendant (Transposed)";
+  } else if(WhichAccessory == "aFive2"){
+    document.getElementById('beltOne').innerHTML = "Dreamy Belt";
+  } else if(WhichAccessory == "aSix2"){
+    document.getElementById('earringOne').innerHTML = "Commanding Force Earring";
+}
+}
+function changeA3(WhichAccessory){ 
+  if(WhichAccessory == "aOne3"){
+    document.getElementById('faceOne').innerHTML = "Twilight Mark";
+    } else if(WhichAccessory == "aTwo3"){
+    document.getElementById('eyeOne').innerHTML = "Papulatus Mark";
+    } else if(WhichAccessory == "aThree3"){
+  document.getElementById('pendantOne').innerHTML = "Source of Suffering";
+  } else if(WhichAccessory == "aFour3"){
+  document.getElementById('pendantTwo').innerHTML = "Source of Suffering";
+  }
+}
+function changeA4(WhichAccessory){ 
+  if(WhichAccessory == "aTwo4"){
+    document.getElementById('eyeOne').innerHTML = "Black Bean Mark";
+    } else if(WhichAccessory == "aThree4"){
+  document.getElementById('pendantOne').innerHTML = "Dominator Pendant";
+  } else if(WhichAccessory == "aFour4"){
+  document.getElementById('pendantTwo').innerHTML = "Dominator Pendant";
+  }
+}
+
+//Armour dropdown functions
+
+function changeAR0(WhichArmour){
+  if(WhichArmour == "arOne0"){
+    document.getElementById('hatOne').innerHTML = "Hat ";
+    } else if(WhichArmour == "arTwo0"){
+    document.getElementById('topOne').innerHTML = "Top ";
+    } else if(WhichArmour == "arThree0"){
+  document.getElementById('bottomOne').innerHTML = "Bottom ";
+  } else if(WhichArmour == "arFour0"){
+  document.getElementById('glovesOne').innerHTML = "Gloves ";
+  } else if(WhichArmour == "arFive0"){
+    document.getElementById('bootsOne').innerHTML = "Boots ";
+  } else if(WhichArmour == "arSix0"){
+    document.getElementById('shoulderOne').innerHTML = "Shoulder ";
+  } else if (WhichArmour == "arSeven0"){
+    document.getElementById('capeOne').innerHTML = "Cape ";
+  }
+}
+
+function changeAR1(WhichArmour){
+  if(WhichArmour == "arOne1"){
+    document.getElementById('hatOne').innerHTML = "Ethereal Hat";
+    } else if(WhichArmour == "arTwo1"){
+    document.getElementById('topOne').innerHTML = "Ethereal Top";
+    } else if(WhichArmour == "arThree1"){
+  document.getElementById('bottomOne').innerHTML = "Ethereal Bottom";
+  } else if(WhichArmour == "arFour1"){
+  document.getElementById('glovesOne').innerHTML = "Ethereal Gloves";
+  } else if(WhichArmour == "arFive1"){
+    document.getElementById('bootsOne').innerHTML = "Ethereal Boots";
+  } else if(WhichArmour == "arSix1"){
+    document.getElementById('shoulderOne').innerHTML = "Ethereal Shoulder";
+  } else if (WhichArmour == "arSeven1"){
+    document.getElementById('capeOne').innerHTML = "Ethereal Cape";
+  }
+}
+
+function changeAR2(WhichArmour){
+  if(WhichArmour == "arOne2"){
+    document.getElementById('hatOne').innerHTML = "Arcane Umbra Hat";
+    } else if(WhichArmour == "arTwo2"){
+    document.getElementById('topOne').innerHTML = "Root Abyss Top";
+    } else if(WhichArmour == "arThree2"){
+  document.getElementById('bottomOne').innerHTML = "Root Abyss Bottom";
+  } else if(WhichArmour == "arFour2"){
+  document.getElementById('glovesOne').innerHTML = "Arcane Umbra Gloves";
+  } else if(WhichArmour == "arFive2"){
+    document.getElementById('bootsOne').innerHTML = "Arcane Umbra Boots";
+  } else if(WhichArmour == "arSix2"){
+    document.getElementById('shoulderOne').innerHTML = "Arcane Umbra Shoulder";
+  } else if (WhichArmour == "arSeven2"){
+    document.getElementById('capeOne').innerHTML = "Arcane Umbra Cape";
+  }
+}
+
+function changeAR3(WhichArmour){
+  if(WhichArmour == "arOne3"){
+    document.getElementById('hatOne').innerHTML = "Absolab Hat";
+    } else if(WhichArmour == "arFour3"){
+  document.getElementById('glovesOne').innerHTML = "Absolab Gloves";
+  } else if(WhichArmour == "arFive3"){
+    document.getElementById('bootsOne').innerHTML = "Absolab Boots";
+  } else if(WhichArmour == "arSix3"){
+    document.getElementById('shoulderOne').innerHTML = "Absolab Shoulder";
+  } else if (WhichArmour == "arSeven3"){
+    document.getElementById('capeOne').innerHTML = "Absolab Cape";
+  }
+}
+
+function changeAR4(WhichArmour){
+  if(WhichArmour == "arOne4"){
+    document.getElementById('hatOne').innerHTML = "Root Abyss Hat";
+    }
+}
+
+function changeAR5(WhichArmour){
+  if(WhichArmour == "arOne5"){
+    document.getElementById('hatOne').innerHTML = "Lucky Hat";
+    }
+}
+
+//WSE dropdown functions
+
+  function changeW0(WhichWSE){
+    if(WhichWSE == "wOne0"){
+      document.getElementById('weaponOne').innerHTML = "Weapon ";
+      } else if(WhichWSE == "wTwo0"){
+      document.getElementById('secondaryOne').innerHTML = "Secondary ";
+      } else if(WhichWSE == "wThree0"){
+    document.getElementById('emblemOne').innerHTML = "Emblem ";
+      }
+    }
+
+    function changeW1(WhichWSE){
+      if(WhichWSE == "wOne1"){
+        document.getElementById('weaponOne').innerHTML = "Genesis Weapon";
+        } else if(WhichWSE == "wTwo1"){
+        document.getElementById('secondaryOne').innerHTML = "Class Secondary";
+        } else if(WhichWSE == "wThree1"){
+      document.getElementById('emblemOne').innerHTML = "Honor Roll Emblem";
+        }
+      }
+
+      function changeW2(WhichWSE){
+        if(WhichWSE == "wOne2"){
+          document.getElementById('weaponOne').innerHTML = "Arcane Umbra Weapon";
+          } else if(WhichWSE == "wThree2"){
+        document.getElementById('emblemOne').innerHTML = "Class Emblem";
+          }
+        }
+
+        function changeW3(WhichWSE){
+          if(WhichWSE == "wOne3"){
+            document.getElementById('weaponOne').innerHTML = "Absolab Weapon";
+            }
+        }
+
+//misc dropdown functions
+
+function changeM0(WhichMisc){
+  if(WhichMisc == "mOne0"){
+    document.getElementById('pocketOne').innerHTML = "Pocket ";
+    } else if(WhichMisc == "mTwo0"){
+    document.getElementById('badgeOne').innerHTML = "Badge ";
+    } else if(WhichMisc == "mThree0"){
+  document.getElementById('medalOne').innerHTML = "Medal ";
+  } else if(WhichMisc == "mFour0"){
+  document.getElementById('androidOne').innerHTML = "Android ";
+  } else if(WhichMisc == "mFive0"){
+    document.getElementById('heartOne').innerHTML = "Heart ";
+  }
+}
+
+function changeM1(WhichMisc){
+  if(WhichMisc == "mOne1"){
+    document.getElementById('pocketOne').innerHTML = "Cursed Red Spellbook";
+    } else if(WhichMisc == "mTwo1"){
+    document.getElementById('badgeOne').innerHTML = "Sengoku Hakase Badge";
+    } else if(WhichMisc == "mThree1"){
+  document.getElementById('medalOne').innerHTML = "Seven Day Monster Parker";
+  } else if(WhichMisc == "mFour1"){
+  document.getElementById('androidOne').innerHTML = "Wondroid";
+  } else if(WhichMisc == "mFive1"){
+    document.getElementById('heartOne').innerHTML = "Wondroid Heart";
+  }
+}
+
+function changeM2(WhichMisc){
+  if(WhichMisc == "mOne2"){
+    document.getElementById('pocketOne').innerHTML = "Cursed Green Spellbook";
+    } else if(WhichMisc == "mTwo2"){
+    document.getElementById('badgeOne').innerHTML = "Ghost Ship Exorcist";
+    } else if(WhichMisc == "mThree2"){
+  document.getElementById('medalOne').innerHTML = "Reboot Master Class";
+  } else if(WhichMisc == "mFour2"){
+  document.getElementById('androidOne').innerHTML = "Regular Android";
+  } else if(WhichMisc == "mFive2"){
+    document.getElementById('heartOne').innerHTML = "Fairy Heart";
+  }
+}
+
+function changeM3(WhichMisc){
+  if(WhichMisc == "mOne3"){
+    document.getElementById('pocketOne').innerHTML = "Cursed Blue Spellbook";
+    } else if(WhichMisc == "mTwo3"){
+    document.getElementById('badgeOne').innerHTML = "Genesis Badge";
+    } else if(WhichMisc == "mThree3"){
+  document.getElementById('medalOne').innerHTML = "Antellion Guardian";
+  } else if(WhichMisc == "mFive3"){
+    document.getElementById('heartOne').innerHTML = "Regular Heart (Below lvl100)";
+  }
+}
+
+function changeM4(WhichMisc){
+  if(WhichMisc == "mOne4"){
+    document.getElementById('pocketOne').innerHTML = "Cursed Yellow Spellbook";
+    } else if(WhichMisc == "mTwo4"){
+    document.getElementById('badgeOne').innerHTML = "Monster Park Badge";
+    }
+}
+
+function changeM5(WhichMisc){
+  if(WhichMisc == "mOne5"){
+    document.getElementById('pocketOne').innerHTML = "Pink Holy Cup";
+    } else if(WhichMisc == "mTwo5"){
+    document.getElementById('badgeOne').innerHTML = "Crystal Ventus Badge";
+    }
+}
+
+//document.body.style.backgroundImage = "url('img_tree.png')";
